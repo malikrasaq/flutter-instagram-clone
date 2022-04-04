2dbfdbf68b433069c1642e2f665bba7fa55e4362
malikrasaq <malikrasaq@gmail.com>
2022-04-05 00:14:20 +0100

    Setup responsive layout(web & mobile)

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({Key? key,required this.webScreenLayout,
+      required this.mobileScreenLayout}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if(constraints.maxWidth > webScreenSize) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
