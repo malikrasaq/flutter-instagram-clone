031f92c63889c49f6e86e5d055c8b53354bec9b2
malikrasaq <malikrasaq@gmail.com>
2022-04-05 08:24:08 +0100

    Added Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/screens/login_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter_instagram_clone/responsive/mobilescreen_layout.dart';
 import 'package:flutter_instagram_clone/responsive/responsive_layout_screen.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: mobileBackgroundColor
 
       ),
-      home: const ResponsiveLayout(webScreenLayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout())
+      home:const LoginScreen(),
     );
   }
 }
