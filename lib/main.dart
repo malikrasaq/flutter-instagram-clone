44bb009ec6c2044250730aa587587dd18fed34c4
malikrasaq <malikrasaq@gmail.com>
2022-04-05 09:15:45 +0100

    Added Signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/screens/login_screen.dart';
+import 'package:flutter_instagram_clone/screens/signup_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter_instagram_clone/responsive/mobilescreen_layout.dart';
 import 'package:flutter_instagram_clone/responsive/responsive_layout_screen.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: mobileBackgroundColor
 
       ),
-      home:const LoginScreen(),
+      home:const SignUpScreen(),
     );
   }
 }
