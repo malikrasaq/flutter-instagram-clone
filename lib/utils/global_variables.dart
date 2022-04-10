abe6f14a392a7c90b19576fa11c35613f3fb28e4
malikrasaq <malikrasaq@gmail.com>
2022-04-10 23:58:14 +0100

    Added feeds screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/screens/add_post_screen.dart';
+import 'package:flutter_instagram_clone/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 
 const homeScreenItems = [
-  Text('Home Screen'),
+  FeedScreen(),
   Text('Search Screen'),
   AddPostScreen(),
   Text('Notifications Screen'),
