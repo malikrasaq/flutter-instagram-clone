26d6762e8e41cc7e219696308a8c9625e62f2a10
malikrasaq <malikrasaq@gmail.com>
2022-04-12 13:09:52 +0100

    Fixed errors in post image upload to firebase storage

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/provider/user_provider.dart';
 import 'package:flutter_instagram_clone/screens/login_screen.dart';
-import 'package:flutter_instagram_clone/screens/signup_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter_instagram_clone/responsive/mobilescreen_layout.dart';
 import 'package:flutter_instagram_clone/responsive/responsive_layout_screen.dart';
